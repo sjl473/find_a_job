770e1eb993e5913be94ee72866d871964c42f5f8
sjl473 <13766899957@163.com>
2021-05-23 16:06:05 +0800

    f

--- leetcode/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/peter_the_greatest/Desktop/leetcode
-# Build directory: /Users/peter_the_greatest/Desktop/leetcode/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
